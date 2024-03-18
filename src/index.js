cb00cc7fbc7a40b3a65d73e58639b290c706ff8d
Ashutosh Ukale <ashutoshukale@gmail.com>
2024-03-18 14:52:34 +0530

    Utils Code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 // require("dotenv").config({ path: "./env" });
 import connectDB from "./db/index.js";
+import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port:${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed!!! ", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {};
+// const asyncHandler = (func) => {() => {}}  function inside a funtion but usually we dont put the brackets
+// const asyncHandler = (func) => () => {};
+// const asyncHandler = (func) => async ( ) => {}
+
+// try catch one
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
